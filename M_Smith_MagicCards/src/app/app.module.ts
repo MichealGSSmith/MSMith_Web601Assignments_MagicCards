248268c32aa04851a2b7c3aaf68bc7eb86cc1e19
MichealGSSmith <w0777761@myscc.ca>
2022-03-27 03:35:35 -0400

    getting hover affects
    trying to get the hover affect to work i have an issue with it im going to see if i can jump ahead without finishing this there is an isue in my hover affect directiv spec file i need to check out

--- M_Smith_MagicCards/src/app/app.module.ts
+++ M_Smith_MagicCards/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { ContentCardComponent } from './content-card/content-card.component';
 import { TypeFilterPipe } from './type-filter.pipe';
+import { HoverAffectDirective } from './hover-affect.directive'; 
 @NgModule({
   declarations: [
     AppComponent,
     ContentListComponent,
     ContentCardComponent,
-    TypeFilterPipe
+    TypeFilterPipe,
+    HoverAffectDirective
   ],
   imports: [
     BrowserModule
